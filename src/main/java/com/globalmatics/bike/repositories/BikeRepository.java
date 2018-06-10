602ea7f2f5854270f6e9d357341cd9bc5855458d
mahadyhasan <mahady.hasan@hotmail.com>
2018-06-10 16:50:38 +0100

    first commit

---
+++ src/main/java/com/globalmatics/bike/repositories/BikeRepository.java
@@ -0,0 +1,9 @@
+package com.globalmatics.bike.repositories;
+
+import com.globalmatics.bike.models.Bike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BikeRepository extends JpaRepository<Bike, Long> {
+
+
+}
